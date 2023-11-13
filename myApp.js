ecf055011bb4e058642ec80465867024eacce946
Kanon11 <kanoncuetcse11@gmail.com>
2023-11-13 18:06:54 +0600

    middleware log

--- myApp.js
+++ myApp.js
@@ -3,7 +3,10 @@ let app = express();
 app.use('/public', express.static(__dirname + '/public'));
 require('dotenv').config()
 
-
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next();
+})
 
 
 
