dd2ed9061dc3d81977bdb728dc71a5cee8970e5b
Kanon11 <kanoncuetcse11@gmail.com>
2023-11-13 17:00:53 +0600

    json

--- myApp.js
+++ myApp.js
@@ -5,6 +5,10 @@ app.use('/public', express.static(__dirname+'/public'));
 app.get("/",(req, res) => {
     res.sendFile(__dirname +'/views/index.html')
 })
+app.get("/json", (req, res) => {
+    // res.send({ message: "Hello json" });
+    res.json({ message: "Hello json" });
+})
 
 
 
