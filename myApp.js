69b56575e6ea61c1b2527cca1292f408da4a7fd9
Kanon11 <kanoncuetcse11@gmail.com>
2023-11-13 23:47:21 +0600

    body parser

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ let express = require('express');
 let app = express();
 app.use('/public', express.static(__dirname + '/public'));
 require('dotenv').config()
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}))
 app.use((req,res,next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next();
